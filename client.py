640f6f5c181b31e4139d3001b43a289d783d5d99
Dreamcatcher <gal1989@mail.ru>
2022-10-09 21:18:43 +0300

    проверка даты

--- client.py
+++ client.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from config import api_url, license_key
 import requests
 from cryptography.fernet import Fernet
@@ -5,29 +7,6 @@ from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 import base64
 import datetime
-
-
-def send_string(dates):
-    key_after_encode = base64.urlsafe_b64encode(kdf.derive(b'bytes'))
-    fer_key = Fernet(key_after_encode)
-    encrypted_key = fer_key.encrypt(bytes(license_key + dates, encoding='utf-8'))
-    e = encrypted_key.decode('utf-8')
-    return e
-
-
-def check_connect(url, b):
-    times = datetime.datetime.now().strftime('%H:%M')
-    if times[3:] == '00':
-        resp = requests.get(f'{url}/check_connect/{b}')
-        print(f'проверка лицензии каждый час {resp}')
-
-
-def constant_check():
-    date = datetime.datetime.now().strftime('%d/%m/%Y %H:%M')
-    a = send_string(date)
-    while True:
-        check_connect(api_url, a)
-
 
 kdf = PBKDF2HMAC(
     algorithm=hashes.SHA256(),
@@ -41,15 +20,38 @@ kdf2 = PBKDF2HMAC(
     salt=b'string',
     iterations=390000,)
 
+key_after_encode = base64.urlsafe_b64encode(kdf.derive(b'bytes'))
+fer_key = Fernet(key_after_encode)
+
+
+def send_string(dates):
+    encrypted_key = fer_key.encrypt(bytes(license_key + dates, encoding='utf-8'))
+    t = encrypted_key.decode('utf-8')
+    return t
+
+
+async def check_connect(url, b):
+    times = datetime.datetime.now().strftime('%H:%M')
+    if times[3:] == '00':
+        resp = requests.get(f'{url}/check_connect/{b}')
+        print(f'проверка лицензии каждый час {resp}')
+    else:
+        print('toko proveril')
+    await asyncio.sleep(55)
+
+
+async def constant_check():
+    while True:
+        dates = datetime.datetime.now().strftime('%d/%m/%Y %H:%M')
+        a = send_string(dates)
+        await check_connect(api_url, a)
+
+
 key_encode = base64.urlsafe_b64encode(kdf2.derive(b'string'))
 respons = Fernet(key_encode)
-
-
 date = datetime.datetime.now().strftime('%d/%m/%Y %H:%M')
 e = send_string(date)
 req = requests.get(f'{api_url}/id_machine/{e}')
-#req = requests.get(f'{api_url}/che')
-print(req.json())
 otvet_client = req.json()['id_machine']
 if otvet_client == 'wrong':
     print('fail')
@@ -63,3 +65,8 @@ else:
     else:
         print('ошибка, вы нарушили')
     print(req.json())
+
+#if __name__ == '__main__':
+#loop = asyncio.get_event_loop()
+#loop.create_task(constant_check())
+asyncio.run(constant_check())
