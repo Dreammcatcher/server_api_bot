d175818d4c50f86524bac361fc53968d84944180
Dreamcatcher <gal1989@mail.ru>
2022-10-25 15:49:24 +0300

    добавил бban

--- run.py
+++ run.py
@@ -1,11 +1,16 @@
 from db import session, LicenseCodes
 import fastapi
 import datetime
-from utils_func import log, create_minut, log_all_information
+from utils_func import create_minut
 from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 import base64
+import logging
+
+logging.basicConfig(level=logging.INFO, filename='logs.log', filemode='a',
+                    format="%(asctime)s %(levelname)s %(message)s", encoding='utf-8')
+
 
 api = fastapi.FastAPI(docs_url=None)
 kdf = PBKDF2HMAC(
@@ -38,26 +43,21 @@ def check_connect(value):
         time_from_client = code_from_client[27:32]
         code_client = code_from_client[:16]
         name_bot_client = code_from_client[32:]
-        print(time_from_client)
-        print(code_client)
-        print(name_bot_client)
     except Exception as er:
-        log_all_information(f'ошибка декодирования строки при ежечасной проверке лицензии - {er}\n')
+        logging.warning(f'ошибка декодирования строки при ежечасной проверке лицензии - {er}')
     keys_from_server = session.query(LicenseCodes.codes).all()
     for i in keys_from_server:
-        print(i)
         if code_client in i:
-            print(1)
             time = datetime.datetime.now().strftime('%H:%M')
             if time_from_client != time:
-                log(license_code=code_client, stroka=f'бот - {name_bot_client} не соответствие времени в коде (клиент-{time_from_client}, сервер-{time}) подмена?\n')
+                logging.warning(f'license_code-{code_client}, бот - {name_bot_client} не соответствие времени (клиент-{time_from_client}, сервер-{time}) подмена?')
             all_minut_now = create_minut(time)
             last_chec_from_bd = session.query(LicenseCodes.last_time_check).filter_by(codes=code_client).first()[0]
             if last_chec_from_bd is not None:
                 all_minut_from_bd = create_minut(last_chec_from_bd)
                 if (all_minut_now - all_minut_from_bd) < 58:
                     #print(f'лицензия с кодом {code_from_client} используется два раза')
-                    log(code_client, last_chec_from_bd, time)
+                    logging.info(code_client, last_chec_from_bd, time)
 
             session.query(LicenseCodes).filter_by(codes=code_client).update({'status': 'in work'})
             session.query(LicenseCodes).filter_by(codes=code_client).update({'last_time_check': time})
@@ -70,7 +70,7 @@ def check_connect(value):
             return {'dont_sniff_my_requests': 'не надо снифить запросы'}
 
     # если кода нет в базе писать в лог
-    log(code_client, stroka=f' бот-{name_bot_client} подбор кода лицензии при часовой проверке')
+    logging.warning(f'code_client-{code_client}, бот-{name_bot_client} подбор кода лицензии при часовой проверке')
 
     return {'dont_sniff_my_requests': '!'}
 
@@ -84,7 +84,7 @@ def check(value):
         code_from_client = code_from_client[:-16]
     except Exception as er:
         date_today = datetime.datetime.today().strftime('%d/%m/%Y %H:%M')
-        log_all_information(f'ошибка декодирования строки при первой проверке лицензии - {er}\n')
+        logging.info(f'ошибка декодирования строки при первой проверке лицензии - {er}')
         wrong_key = response.encrypt(bytes(date_today + 'wrongdecod', encoding='utf-8'))
         wrong_string = wrong_key.decode('utf-8')
         return {'id_machine': wrong_string}
@@ -106,7 +106,7 @@ def check(value):
 def wrong_time(value):
     decrypted_key = fer_key.decrypt(value)
     wrong_from_client = decrypted_key.decode('utf-8')
-    log(wrong_from_client, stroka='с не одинаковым временем')
+    logging.warning(f'ошибка - {wrong_from_client} у клиента время сервера не совпало с временем машины (часы)')
 
 
 # перебор кодов лицензии при первом запуске
@@ -114,4 +114,4 @@ def wrong_time(value):
 def wrong_code(value):
     decrypted_key = fer_key.decrypt(value)
     wrong_from_client = decrypted_key.decode('utf-8')
-    log(wrong_from_client[:-16], stroka='перебор кодов лицензии')
+    logging.warning(f'{wrong_from_client}, перебор кодов лицензии при первом запуске')
